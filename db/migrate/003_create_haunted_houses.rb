6f7fabe49efbbd0fc75a9840e1dbd5ddcba78a03
Jess Rezac <jess.rezac@gmail.com>
2019-07-25 17:18:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStores < ActiveRecord::Migration[5.2]
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
