0003db03f082da13b77745a87ac4b116eb66e675
Jess Rezac <jess.rezac@gmail.com>
2019-07-25 17:11:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.string :image_url
       t.integer :size
-      t.timestamp :date_created
-      t.timestamp :last_updated
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
